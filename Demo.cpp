7bee7eac582911aea82953899f0c4fdcfbfb8db2
Alpha-source101 <ihaiderali909@gmail.com>
2026-05-14 22:01:00 +0500

    Writing little script

--- Demo.cpp
+++ Demo.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello World"<<endl;
+    cout<<"Hello World ."<<endl;
     cout<<" I am writing my first cpp file in Github Server ."<<endl;
     cout<<"Best of Luck!  BYE " ;
 }
